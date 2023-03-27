2d413e6a185048012d963145d4a8c7e3803b76fb
Brijesh Munjapra <103625896+brijeshmunjapra@users.noreply.github.com>
2023-03-27 10:24:28 +0530

    Delete src directory

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA8DbsyQ8vUD91f8D76xVMGF1OJLmirjjU",
-  authDomain: "react-firebase-auth-7d133.firebaseapp.com",
-  projectId: "react-firebase-auth-7d133",
-  storageBucket: "react-firebase-auth-7d133.appspot.com",
-  messagingSenderId: "506504669434",
-  appId: "1:506504669434:web:dac09b95e81515a14c0143",
-};
