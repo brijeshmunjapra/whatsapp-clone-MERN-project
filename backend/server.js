b95b50d48ff01f18eb1883bc15e0f4df1f46f862
brijeshmunjapra <brijeshmunjapra@gmail.com>
2023-03-28 17:38:51 +0530

    pusher added

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,58 @@
 import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 9000;
 // mongodb+srv://brijesh:<password>@cluster0.ozbvkso.mongodb.net/?retryWrites=true&w=majority
 // G5rSBQ5vkSH1mV3N
+const pusher = new Pusher({
+  appId: "1574979",
+  key: "eb290f24781cde35e3bd",
+  secret: "339313096d6e4d32e6f8",
+  cluster: "ap2",
+  useTLS: true,
+});
+
 const connection_url =
   "mongodb+srv://brijesh:G5rSBQ5vkSH1mV3N@cluster0.ozbvkso.mongodb.net/whatsapp?retryWrites=true&w=majority";
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("db connected");
+  const msgCollection = db.collection("messages");
+  const chnageStream = msgCollection.watch();
+
+  chnageStream.on("change", (change) => {
+    console.log(change, "change"); 
+
+    if(change.operationType === 'insert'){
+        const messageDetails = change.fullDocument;
+        pusher.trigger('messages', 'inserted',
+        {
+            name: messageDetails.name,
+            message: messageDetails.message
+        }
+        )
+    }else{
+        console.log('error pusher')
+    }
+
+  });
+});
+
 //app config
 app.get("/", (req, res) => res.status(200).send("Hello"));
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 //db config
 app.post("/messages/new", async (req, res) => {
@@ -28,7 +65,7 @@ app.post("/messages/new", async (req, res) => {
   }
 });
 
-app.get("/messages/new", async (req, res) => {
+app.get("/messages/sync", async (req, res) => {
   try {
     const messages = await Messages.find({});
     return res.status(200).send(messages);
--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
 import ChatComp from "./Components/ChatComp";
+import { useEffect } from "react";
+import Pusher from "pusher-js"
 
 function App() {
+  useEffect(() => {
+  
+    const pusher = new Pusher('eb290f24781cde35e3bd', {
+      cluster: 'ap2'
+    });
+  
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="app">
       <div className="app_body">
